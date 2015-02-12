be18113a67c066d9586c24b6a0d9a4a372d0c70e
Harold Portocarrero <harold.port@gmail.com>
2015-02-12 17:54:57 -0500

    inicio de vinculacion de curso

---
+++ src/main/java/gestion/academica/dao/CursoDao.java
@@ -0,0 +1,36 @@
+package gestion.academica.dao;
+
+import java.util.List;
+
+import gestion.academica.modelo.Curso;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Stateless
+public class CursoDao extends Generico<Curso> {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public CursoDao() {
+        super(Curso.class);
+    }
+	
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+    
+    /**
+     * Listar todos los cursos activos
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+	public List<Curso> listarCursos() {
+        String sql = "SELECT c FROM Curso c WHERE c.estado.idEstado = 1 ORDER BY c.idCurso";
+        return this.getEntityManager().createQuery(sql).getResultList();
+    }
+
+}
